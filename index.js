1173cf918671477d7efdf16a0c809b72506b8cf1
Chelsea Crowson <chelsea.crowson@me.com>
2019-02-27 12:51:09 -0600

    fix footer per page

--- index.js
+++ index.js
@@ -13,10 +13,21 @@ function linkColor(event) {
     event.target.style.color = 'white'
 }
 
+function h1Color(event) {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    event.target.style.color = color
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     // let navLink = document.getElementsByClassName('navLink')
     // for (let link of navLink) {
     //     link.addEventListener('click', linkColor)
     // }
+
+    let h1 = document.getElementsByTagName('h1')
+    h1[0].addEventListener('click', h1Color)
 })
