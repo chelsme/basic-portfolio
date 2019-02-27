bb46b2947a4fe71e2b5258908c0c67b29d8ec438
Chelsea Crowson <chelsea.crowson@me.com>
2019-02-27 12:22:37 -0600

    hover text and nav background color

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-console.log('heyo')
+console.log('Welcome to my portfolio!')
 
+function linkColor(event) {
+    // event.preventDefault()
+    let navLink = document.getElementsByClassName('navLink')
+
+    for (let item of navLink) {
+        item.style.background = 'white'
+        item.style.color = 'black'
+    }
+
+    event.target.style.background = 'darkred'
+    event.target.style.color = 'white'
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    let h1 = document.getElementById('header')
-    h1.addEventListener('click', function (event) {
-        h1.innerText = 'I got hacked!'
-        h1.style.color = 'red'
-        h1.style.background = 'black'
-    })
+    // let navLink = document.getElementsByClassName('navLink')
+    // for (let link of navLink) {
+    //     link.addEventListener('click', linkColor)
+    // }
 })
